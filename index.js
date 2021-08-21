7eac8065c2d62626ec5252e578b88e3d39589fef
Brian Diaz <bidiaz101@gmail.com>
2021-08-20 21:02:02 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const fetchRequest = fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+        .then(resp => resp.json())
+        .then(data => {
+            const p = document.createElement("p")
+            p.innerHTML = data.id
+            document.querySelector("body").appendChild(p)
+        })
+        .catch(error => {
+            const p = document.createElement("p")
+            p.innerHTML = error
+            document.querySelector("body").appendChild(p)
+        })
+    return fetchRequest;
+};
